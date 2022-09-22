e50df7c50bc0acdc6e4c6de087ddfcf46546e96b
phs9903 <phs9903@gmail.com>
2022-09-22 17:55:11 +0900

    삭제 기능 수정, 책 추가 기능,  수정 기능 필요

--- hsbooks/public/script.js
+++ hsbooks/public/script.js
@@ -3,16 +3,17 @@ function deleteBook(id) {
     .then((res) => {
       if (res.ok) {
         alert('삭제되었습니다.');
+        location.reload();
       } else {
-        alert('오류가 발생하였습니다.');
+        alert('오류가 발생하였습니다.1');
       }
     })
     .catch((err) => {
       console.error(err);
-      alert('오류가 발생하였습니다.');
+      alert('오류가 발생하였습니다.2');
     });
 }
 
-const deleteBook1 = async function(id) {
-  const dbook = await axios.delete('/'+id);
-}
+// const deleteBook1 = async function(id) {
+//   const dbook = await axios.delete('/'+id);
+// }
--- hsbooks/routes/index.js
+++ hsbooks/routes/index.js
@@ -26,6 +26,45 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.get('/add', async (req, res) => {
+  try {
+    const books = await Book.findAll({
+      include: {
+        model: Category,
+      },
+      order: [['id', 'DESC']],
+    });
+    const categories = await Category.findAll({
+      order: [['id', 'ASC']],
+    });
+    res.render('add', {
+      title: 'HSBooks',
+      books: books,
+      categories: categories,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const add = await Book.create({
+      title: req.body.title,
+      author: req.body.author,
+      publisher: req.body.publisher,
+      published_date: req.body.published_date,
+      price: req.body.price,
+      CategoryId: req.body.CategoryId,
+    });
+    res.redirect('/add');
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
 router.delete('/:id', async (req, res, next) => {
   try {
     const dbook = await Book.destroy({ where: {id: req.params.id } });
