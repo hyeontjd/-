435f869e24316172ff568fb0e6b5d53fb0a91cfd
박현성 <phs9903@gmail.com>
2022-10-02 17:48:19 +0900

    모듈식 라우터로 수정, 수정 시 placehold에서 value로 값 자체를 가지도록 수정

--- hsbooks/routes/index.js
+++ hsbooks/routes/index.js
@@ -9,23 +9,58 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get('/', async (req, res, next) => {
-  try {
-    const books = await Book.findAll({
-      include: {
-        model: Category,
-      },
-      order: [['id', 'ASC']],
-    });
-    res.render('main', {
-      title: 'HSBooks',
-      books: books,
-    });
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
+router.route('/')
+  .get(async (req, res, next) => {
+    try {
+      const books = await Book.findAll({
+        include: {
+          model: Category,
+        },
+        order: [['id', 'ASC']],
+      });
+      res.render('main', {
+        title: 'HSBooks',
+        books: books,
+      });
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      const add = await Book.create({
+        title: req.body.title,
+        author: req.body.author,
+        publisher: req.body.publisher,
+        published_date: req.body.published_date,
+        price: req.body.price,
+        CategoryId: req.body.CategoryId,
+      });
+      res.redirect('/add');
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  });
+
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const books = await Book.findAll({
+//       include: {
+//         model: Category,
+//       },
+//       order: [['id', 'ASC']],
+//     });
+//     res.render('main', {
+//       title: 'HSBooks',
+//       books: books,
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     next(err);
+//   }
+// });
 
 router.get('/add', async (req, res) => {
   try {
@@ -49,22 +84,22 @@ router.get('/add', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res, next) => {
-  try {
-    const add = await Book.create({
-      title: req.body.title,
-      author: req.body.author,
-      publisher: req.body.publisher,
-      published_date: req.body.published_date,
-      price: req.body.price,
-      CategoryId: req.body.CategoryId,
-    });
-    res.redirect('/add');
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
+// router.post('/', async (req, res, next) => {
+//   try {
+//     const add = await Book.create({
+//       title: req.body.title,
+//       author: req.body.author,
+//       publisher: req.body.publisher,
+//       published_date: req.body.published_date,
+//       price: req.body.price,
+//       CategoryId: req.body.CategoryId,
+//     });
+//     res.redirect('/add');
+//   } catch (err) {
+//     console.error(err);
+//     next(err);
+//   }
+// });
 
 router.get('/update/:id', async (req, res) => {
   try {
@@ -90,64 +125,124 @@ router.get('/update/:id', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res, next) => {
-  try {
-    const ubook = await Book.update({
-      title: req.body.title,
-      author: req.body.author,
-      publisher: req.body.publisher,
-      published_date: req.body.published_date,
-      price: req.body.price,
-      CategoryId: req.body.CategoryId,
-    }, { where: {id: req.params.id } });
-    res.redirect('/');
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
+router.route('/:id')
+  .put(async (req, res, next) => {
+    try {
+      const ubook = await Book.update({
+        title: req.body.title,
+        author: req.body.author,
+        publisher: req.body.publisher,
+        published_date: req.body.published_date,
+        price: req.body.price,
+        CategoryId: req.body.CategoryId,
+      }, { where: {id: req.params.id } });
+      res.redirect('/');
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      const dbook = await Book.destroy({ where: {id: req.params.id } });
+      res.json('delete');
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  });
 
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const dbook = await Book.destroy({ where: {id: req.params.id } });
-    res.json('delete');
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
+// router.put('/:id', async (req, res, next) => {
+//   try {
+//     const ubook = await Book.update({
+//       title: req.body.title,
+//       author: req.body.author,
+//       publisher: req.body.publisher,
+//       published_date: req.body.published_date,
+//       price: req.body.price,
+//       CategoryId: req.body.CategoryId,
+//     }, { where: {id: req.params.id } });
+//     res.redirect('/');
+//   } catch (err) {
+//     console.error(err);
+//     next(err);
+//   }
+// });
 
-router.get('/domain', async (req, res) => {
-  try {
-    const books = await Book.findAll({
-      include: {
-        model: Category,
-      },
-      order: [['id', 'ASC']],
-    });
-    const domains = await Domain.findAll();
-    res.render('domain', {
-      title: 'HSBooks',
-      books: books,
-      domains: domains,
-    });
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
+// router.delete('/:id', async (req, res, next) => {
+//   try {
+//     const dbook = await Book.destroy({ where: {id: req.params.id } });
+//     res.json('delete');
+//   } catch (err) {
+//     console.error(err);
+//     next(err);
+//   }
+// });
 
-router.post('/domain', async (req, res, next) => {
-  try {
-    await Domain.create({
-      host: req.body.host,
-      clientSecret: uuidv4(),
-    });
-    res.redirect('/domain');
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
+router.route('/domain')
+  .get(async (req, res) => {
+    try {
+      const books = await Book.findAll({
+        include: {
+          model: Category,
+        },
+        order: [['id', 'ASC']],
+      });
+      const domains = await Domain.findAll();
+      res.render('domain', {
+        title: 'HSBooks',
+        books: books,
+        domains: domains,
+      });
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      await Domain.create({
+        host: req.body.host,
+        clientSecret: uuidv4(),
+      });
+      res.redirect('/domain');
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  });
+
+// router.get('/domain', async (req, res) => {
+//   try {
+//     const books = await Book.findAll({
+//       include: {
+//         model: Category,
+//       },
+//       order: [['id', 'ASC']],
+//     });
+//     const domains = await Domain.findAll();
+//     res.render('domain', {
+//       title: 'HSBooks',
+//       books: books,
+//       domains: domains,
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     next(err);
+//   }
+// });
+
+// router.post('/domain', async (req, res, next) => {
+//   try {
+//     await Domain.create({
+//       host: req.body.host,
+//       clientSecret: uuidv4(),
+//     });
+//     res.redirect('/domain');
+//   } catch (err) {
+//     console.error(err);
+//     next(err);
+//   }
+// });
 
 module.exports = router;
