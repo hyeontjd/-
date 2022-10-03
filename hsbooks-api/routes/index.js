70809e8adb4db11c29b657ede53ec0568bd65214
박현성 <phs9903@gmail.com>
2022-10-03 22:55:59 +0900

    style에 div width 추가

--- hsbooks-api/routes/index.js
+++ hsbooks-api/routes/index.js
@@ -7,20 +7,20 @@ const URL = 'http://localhost:5000/open';
 axios.defaults.headers.origin = 'http://localhost:5001';
 const request = async (req, api) => {
   try {
-    if (!req.session.jwt) { // 세션에 토큰이 없으면
+    if (!req.session.jwt) {
       const tokenResult = await axios.post(`${URL}/token`, {
         clientSecret: process.env.CLIENT_SECRET,
       });
-      req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
+      req.session.jwt = tokenResult.data.token;
     }
     return await axios.get(`${URL}${api}`, {
       headers: { authorization: req.session.jwt },
-    }); // API 요청
+    });
   } catch (error) {
-    if (error.response.status === 419) { // 토큰 만료시 토큰 재발급 받기
+    if (error.response.status === 419) {
       delete req.session.jwt;
       return request(req, api);
-    } // 419 외의 다른 에러면
+    }
     return error.response;
   }
 };
