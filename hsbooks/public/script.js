cc70a7a75af2044c236a801e6b4da2cb8dc89b6a
박현성 <phs9903@gmail.com>
2022-10-09 17:32:40 +0900

    안쓰는 함수 주석처리

--- hsbooks/public/script.js
+++ hsbooks/public/script.js
@@ -1,15 +1,16 @@
-function deleteBook(id) {
-  fetch('/'+id, {method: 'delete'})
-    .then((res) => {
-      if (res.ok) {
-        alert('삭제되었습니다.');
-        location.reload();
-      } else {
-        alert('오류가 발생하였습니다.1');
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      alert('오류가 발생하였습니다.2');
-    });
-}
+// function deleteBook(id) {
+//   fetch('/'+id, {method: 'delete'})
+//     .then((res) => {
+//       if (res.ok) {
+//         alert('삭제되었습니다.');
+//         location.reload();
+//       } else {
+//         alert('오류가 발생하였습니다.1');
+//       }
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       alert('오류가 발생하였습니다.2');
+//     });
+// }
+
